84f483ba424bb6543b2c9c2cfa2f5122d179ee9a
CrossTM <jake-crossin@hotmail.com>
2025-03-25 16:04:27 +1100

    Adding New Mod KillMimics
    Opens all NonMimic Chests, AutoSelects the x4 Perfect Chest, and completes the Chest Event

---
+++ KillMimics/Settings.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx.Configuration;
+using IdleSlayerMods.Common.Config;
+
+namespace KillMimics;
+
+internal sealed class Settings(ConfigFile cfg) : BaseConfig(cfg)
+{
+    internal ConfigEntry<bool> ShouldRevealMultipliers;
+    internal ConfigEntry<bool> ShouldRevealDuplicator;
+    internal ConfigEntry<bool> ShouldRevealArmoryChest;
+
+    protected override void SetBindings()
+    {
+        ShouldRevealMultipliers = Bind("General", "ShouldRevealMultipliers", false,
+            "Should reveal multipliers in chest hunt");
+        ShouldRevealDuplicator = Bind("General", "ShouldRevealDuplicator", false,
+            "Should reveal duplicator item in chest hunt");
+        ShouldRevealArmoryChest = Bind("General", "ShouldRevealArmoryChest", false,
+            "Should reveal armory chest in chest hunt");
+    }
+}
