c01788ee2da2e935e7fdfbdde5faf2babf513227
CrossTM <jake-crossin@hotmail.com>
2025-04-09 08:43:33 +1000

    updated mod name

--- PerfectChestHunter/MyPluginInfo.cs
+++ PerfectChestHunter/MyPluginInfo.cs
@@ -3,7 +3,7 @@ namespace PerfectChestHunter
     public static class MyPluginInfo
     {
         public const string PLUGIN_GUID = "PerfectChestHunter";
-        public const string PLUGIN_NAME = "Perfect CHest Hunter";
+        public const string PLUGIN_NAME = "Perfect Chest Hunter";
         public const string PLUGIN_VERSION = "1.1.1";
         public const string PLUGIN_AUTHOR = "CrossTM";
     }
