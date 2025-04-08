ad29d321e2cbdcf8d264f1ff4ee5f39882f3cfbd
CrossTM <jake-crossin@hotmail.com>
2025-04-09 08:33:38 +1000

    updated version number

--- PerfectChestHunter/MyPluginInfo.cs
+++ PerfectChestHunter/MyPluginInfo.cs
@@ -4,7 +4,7 @@ namespace PerfectChestHunter
     {
         public const string PLUGIN_GUID = "PerfectChestHunter";
         public const string PLUGIN_NAME = "Perfect CHest Hunter";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.1.1";
         public const string PLUGIN_AUTHOR = "CrossTM";
     }
 }
