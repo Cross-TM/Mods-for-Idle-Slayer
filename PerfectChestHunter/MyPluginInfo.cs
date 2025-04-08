1fb77bade7c88bf5689b9567dfbdca94291529aa
CrossTM <jake-crossin@hotmail.com>
2025-04-09 08:31:49 +1000

    REnamed Plugin Name for Perfect Chest Hunter

--- PerfectChestHunter/MyPluginInfo.cs
+++ PerfectChestHunter/MyPluginInfo.cs
@@ -3,7 +3,7 @@ namespace PerfectChestHunter
     public static class MyPluginInfo
     {
         public const string PLUGIN_GUID = "PerfectChestHunter";
-        public const string PLUGIN_NAME = "Chest Hunt Revealer";
+        public const string PLUGIN_NAME = "Perfect CHest Hunter";
         public const string PLUGIN_VERSION = "1.1.0";
         public const string PLUGIN_AUTHOR = "CrossTM";
     }
