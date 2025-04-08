9d08bfa48b44c11335a1c5c3b2f939eb1cbee02c
CrossTM <jake-crossin@hotmail.com>
2025-04-09 00:27:08 +1000

    Mod created to bypass cloud sync and start game immediately

---
+++ NoSyncGameLoad/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ExperimentalMods;
+
+public static class MyPluginInfo
+{
+    public const string PLUGIN_GUID = "NoSyncGameLoad";
+    public const string PLUGIN_NAME = "No Sync Game Load";
+    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_AUTHOR = "CrossTM";
+}
---
+++ NoSyncGameLoad/Plugin.cs
@@ -0,0 +1,39 @@
+using IdleSlayerMods.Common;
+using MelonLoader;
+using System.Reflection;
+using MyPluginInfo = ExperimentalMods.MyPluginInfo;
+using Plugin = ExperimentalMods.Plugin;
+
+[assembly: MelonInfo(typeof(Plugin), MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION, MyPluginInfo.PLUGIN_AUTHOR)]
+[assembly: MelonAdditionalDependencies("IdleSlayerMods.Common")]
+
+namespace ExperimentalMods;
+
+public class Plugin : MelonMod
+{
+//    internal static ConfigFile Config;
+    internal static readonly MelonLogger.Instance Logger = Melon<Plugin>.Logger;
+    internal static ModHelper ModHelperInstance;
+
+    public override void OnInitializeMelon()
+    {
+//        Config = new(MyPluginInfo.PLUGIN_GUID);
+        Logger.Msg($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        ModHelper.ModHelperMounted += SetModHelperInstance;
+
+//        var harmony = new HarmonyLib.Harmony(MyPluginInfo.PLUGIN_NAME);
+//        harmony.PatchAll();
+
+    }
+    private static void SetModHelperInstance(ModHelper instance) => ModHelperInstance = instance;
+
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        if (sceneName == "Title Screen")
+        {
+            ModUtils.RegisterComponent<GameStart>();
+        }
+        
+        if (sceneName != "Game") return;
+    }
+}
