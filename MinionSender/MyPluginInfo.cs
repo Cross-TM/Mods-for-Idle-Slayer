2d00a084a7f0d6d0d98ae36ff4d72e951ed91405
CrossTM <jake-crossin@hotmail.com>
2025-04-14 03:10:46 +1000

    Updated compatibility for IdleCommons 1.1.8

--- MinionSender/MyPluginInfo.cs
+++ MinionSender/MyPluginInfo.cs
@@ -4,7 +4,7 @@
     {
         public const string PLUGIN_GUID = "MinionSender";
         public const string PLUGIN_NAME = "Minion Sender";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.1.1";
         public const string PLUGIN_AUTHOR = "CrossTM";
     }
 }
