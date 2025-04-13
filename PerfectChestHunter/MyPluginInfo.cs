8645c89acebd5526f54d0928ea44cb59099cc07f
CrossTM <jake-crossin@hotmail.com>
2025-04-14 03:10:18 +1000

    Updated compatibility for IdleCommons 1.1.8

--- PerfectChestHunter/MyPluginInfo.cs
+++ PerfectChestHunter/MyPluginInfo.cs
@@ -4,7 +4,7 @@ namespace PerfectChestHunter
     {
         public const string PLUGIN_GUID = "PerfectChestHunter";
         public const string PLUGIN_NAME = "Perfect Chest Hunter";
-        public const string PLUGIN_VERSION = "1.1.1";
+        public const string PLUGIN_VERSION = "1.1.2";
         public const string PLUGIN_AUTHOR = "CrossTM";
     }
 }
