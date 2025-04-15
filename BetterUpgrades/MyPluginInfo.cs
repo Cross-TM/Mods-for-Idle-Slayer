98bfd41d60b871ec5b2c32a89cb14101ccf861f2
CrossTM <jake-crossin@hotmail.com>
2025-04-16 02:09:17 +1000

    New Mod Created

---
+++ BetterUpgrades/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Betterupgrades;
+
+public static class MyPluginInfo
+{
+    public const string PLUGIN_GUID = "Betterupgrades";
+    public const string PLUGIN_NAME = "Betterupgrades";
+    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_AUTHOR = "CrossTM";
+}
