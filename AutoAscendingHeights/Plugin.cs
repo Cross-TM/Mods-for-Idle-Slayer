4cdf385792022002a39dfa2f753a670312aa811e
CrossTM <jake-crossin@hotmail.com>
2025-04-22 14:28:29 +1000

    AutoAscendingHeights mod created and patched

--- AutoAscendingHeights/Plugin.cs
+++ AutoAscendingHeights/Plugin.cs
@@ -11,17 +11,22 @@ namespace AutoAscendingHeights;
 public class Plugin : MelonMod
 {
     internal static ConfigFile Config;
+    internal static ModHelper ModHelperInstance;
+
     internal static readonly MelonLogger.Instance Logger = Melon<Plugin>.Logger;
-    
+
     public override void OnInitializeMelon()
     {
         Config = new(MyPluginInfo.PLUGIN_GUID);
         Logger.Msg($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        ModHelper.ModHelperMounted += SetModHelperInstance;
+
     }
+    private static void SetModHelperInstance(ModHelper instance) => ModHelperInstance = instance;
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
         if (sceneName != "Game") return;
-        ModUtils.RegisterComponent<MyBehaviour>();
+        ModUtils.RegisterComponent<AutoAscend>();
     }
 }
