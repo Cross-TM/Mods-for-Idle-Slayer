b1d7753721b9746d6dc6b436d958671609758a31
Maycon Queiros <queiros.maycon@gmail.com>
2025-09-20 00:07:27 -0300

    Update version to 1.2.0 and revise author information

--- Enhanced_Quests_Local/MyPluginInfo.cs
+++ Enhanced_Quests_Local/MyPluginInfo.cs
@@ -4,6 +4,6 @@ public static class MyPluginInfo
 {
     public const string PLUGIN_GUID = "Enhanced_Quests_Local";
     public const string PLUGIN_NAME = "Enhanced_Quests_Local";
-    public const string PLUGIN_VERSION = "1.1.1";
-    public const string PLUGIN_AUTHOR = "Shokram - CrossTM - Watashinoushi";
+    public const string PLUGIN_VERSION = "1.2.0";
+    public const string PLUGIN_AUTHOR = "Shokram";
 }
